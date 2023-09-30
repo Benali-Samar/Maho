ebe67ca66f60e1d124a5e84e394ec97119020f70
BenAli Samar <samar.benali@istic.ucar.tn>
2023-09-30 21:36:50 +0100

    Fixed return to new line and added some comments

--- maho.c
+++ maho.c
@@ -5,22 +5,27 @@
 #include <ctype.h> // To control the printable characters
 
 struct termios orig_termios;
-
+// Disable the raw mode when finishing writing else it will not exit the section
 void disableRawMode(){
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
-
+// enabling raw mode that can display characters directly while writing, oppositely to canonical mode that waits for a specified  signal to print all typed characters. 
+// Canonical mode is the default so we should enable the raw mode instead.
 void enableRawMode()
 {
   
   tcgetattr(STDIN_FILENO, &orig_termios); // to read the current attributes into a struct
-  atexit(disableRawMode) ; // used to disable raw mode function 
+  atexit(disableRawMode) ; // after read disable and comback to canonical mode for signals use
   
   struct termios raw = orig_termios;
   raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);  // disable CTRL+S and CTRL+Q and miscellaneous flags
   raw.c_oflag &= ~(OPOST); // disable all output processing like "\n"
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // modifying the struct by hand 
+
+ // The c_cc stands for "control characters" an array of bytes to control various terminal settings. 
+  // Set the min number of bytes of input needed before read () can return.
   raw.c_cc[VMIN] = 0; 
+  //  Set the max amount of time to wait before read() returns.
   raw.c_cc [VTIME] = 1;
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // to write the new terminal attributes back out
@@ -30,19 +35,26 @@ void enableRawMode()
 
 int main ()
 {
+  // All the stuff did upside
   enableRawMode();
 
-  
-  while(1) {
+  // infinit loop till the user type 'q'
   char c = '\0';
-  read(STDIN_FILENO, &c, 1);
-  if (iscntrl(c))  
-  {
+  while(1) {
+    
+    // Reading ... 
+    read(STDIN_FILENO, &c, 1);
+    // does c a controle character ?
+    if (iscntrl(c))  
+    { 
+      // print it 
       printf("%d\n", c);
-  }else {
-      printf("%d ('%c')\n", c, c);
+    }else {
+      //else to go back in a new line write the whole "\r\n"
+      printf("%d ('%c')\r\n", c, c);
     }
-  if (c == 'q') break;  
+    if (c == 'q')
+      break;  
   }
   return 0;
 }
