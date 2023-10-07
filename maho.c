bdae5bbdeacc326fe374a1e53d7dea12a5cb5108
BenAli Samar <samar.benali@istic.ucar.tn>
2023-10-07 23:59:09 +0100

    Window size sure

--- maho.c
+++ maho.c
@@ -98,6 +98,35 @@ char editorReadKey()
   return c;
 }
 
+
+
+//Get Cursor position 
+int getCursorPosition(int *rows,int *cols)
+{
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO,"\x1b[6n",4 )!= 4)
+    return -1;
+
+   
+  while (i < sizeof(buf) -1){
+    if (read (STDIN_FILENO, &buf[i] ,1)!=1)
+      break;
+    if (buf[i] == 'R')
+      break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if(buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2],"%d;%d",rows,cols) != 2) return -1;
+  
+  return 0;
+}
+ 
+
+
 //Get window size with IOCTL built in request: TIOCGWINSZ 
 // Stands for Input/Output Control Get WINdow size
 
@@ -106,8 +135,12 @@ int getWindowSize(int *rows, int *cols)
   struct winsize ws;
   if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)== -1 || ws.ws_col == 0)
   {
-    return -1;
-  }else{
+    //To move the cursor to the bottom-right corner
+    //The 999 is to say the largest value to move it to the right ,The C to say move forward,the B is to say move it down.
+    if (write(STDOUT_FILENO,"\x1b[999C\x1b[999B",12) != 12)
+      return -1;
+    return getCursorPosition (rows,cols);
+  }else{  
     *cols = ws.ws_col;
     *rows = ws.ws_row;
     return 0;
