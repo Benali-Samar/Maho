e088e3232c860dfa0f989fa33093b4d34961347c
BenAli Samar <samar.benali@istic.ucar.tn>
2023-10-08 02:06:51 +0100

    key manip cursor (still bugguie)

--- maho.c
+++ maho.c
@@ -39,7 +39,14 @@ struct abuf {
   int len;
 };
 
-
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT ,
+  ARROW_UP,
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
+};
 
 
 /***** Terminal *****/
@@ -95,7 +102,7 @@ void enableRawMode()
 
 
 //Reading keypress in lowleveel
-char editorReadKey()
+int editorReadKey()
 {
   int nread;
   char c;
@@ -104,7 +111,39 @@ char editorReadKey()
     if (nread == -1 && errno != EAGAIN)
       die("read");
   }
-  return c;
+
+  if (c == '\x1b')
+  {
+    char seq[3];
+
+    if(read(STDIN_FILENO,&seq[0],1) != 1) return '\x1b';
+    if(read(STDIN_FILENO,&seq[1],1) != 1) return '\x1b';
+
+    if (seq[0] == '[')
+    {
+      if (seq[1] >= '0' && seq[1] <= '9')
+      {
+        if (read(STDIN_FILENO,&seq[2],1) != 1) return '\x1b';
+        if(seq[2] == '~'){
+          switch(seq[1]){
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+          }
+        }
+      }else {
+          switch (seq[1])
+          {
+            case 'A': return ARROW_UP;
+            case 'B': return ARROW_DOWN;
+            case 'C': return ARROW_RIGHT;
+            case 'D': return ARROW_LEFT;
+          }
+      }
+  }
+    return '\x1b';
+  }else {
+    return c;
+  }
 }
 
 
@@ -180,20 +219,22 @@ void abFree(struct abuf *ab)
 /***** Inputs *****/ 
 
 //Cursor moves 
-void editorMoveCursor(char key)
+void editorMoveCursor(int key)
 {
   switch(key){
-    case'a':
-      E.cx--;
+    case ARROW_LEFT:
+      if (E.cx > 0){
+        E.cx--;
+      }
       break;
-    case 'd':
-      E.cx++;
+    case ARROW_RIGHT:
+      if (E.cx < E.screencols -1) E.cx++;
       break;
-    case'w':
-      E.cy--;
+    case ARROW_UP:
+      if (E.cy > 0)E.cy--;
       break;
-    case 's':
-      E.cy++;
+    case ARROW_DOWN:
+      if (E.cy < E.screenrows -1) E.cy ++;
       break;
   }
 }
@@ -201,7 +242,7 @@ void editorMoveCursor(char key)
 //Mapping the keypress to editor operations
 void editorProcessKeypress()
 {
-  char c = editorReadKey();
+  int c = editorReadKey();
 
   switch(c) 
   {
@@ -210,10 +251,20 @@ void editorProcessKeypress()
       write(STDOUT_FILENO, "\x1b[H" , 3);
       exit(0);
       break;
-    case 'w':
-    case 's':
-    case 'a':
-    case 'd':
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenrows;
+        while (times--)
+               editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      }
+    break;
+
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
       editorMoveCursor(c);
       break;
   }
@@ -315,7 +366,7 @@ int main ()
         }
 
     //Process the characters	  
-    editorProcessKeypress(c);
+    editorProcessKeypress();
     //Flush the screen
     editorRefreshscreen(); 
   }
