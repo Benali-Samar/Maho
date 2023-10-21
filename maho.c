f719a1654d33b7d3d1a2ba02a5f994f7f82c08da
BenAli Samar <samar.benali@istic.ucar.tn>
2023-10-21 22:00:43 +0100

    visibility of the text while opening

--- maho.c
+++ maho.c
@@ -40,7 +40,7 @@ struct editorConfig {
   int screenrows;
   int screencols;
   int numrows;
-  erow row;
+  erow *row;
   struct termios orig_termios;
 };
 
@@ -211,6 +211,22 @@ int getWindowSize(int *rows, int *cols)
   }
 }
 
+
+/*** row operations ***/
+void editorAppendRow(char *s, size_t len)
+{
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  int at = E.numrows;
+
+  E.row[at].size = len;
+  E.row[at].chars = malloc (len+1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+  E.numrows++ ;
+}
+
+
+
 /*** file i/o ***/
 void editorOpen(char *filename) {
   FILE *fp = fopen(filename, "r");
@@ -219,15 +235,11 @@ void editorOpen(char *filename) {
   size_t linecap = 0;
   ssize_t linelen;
   linelen = getline(&line, &linecap, fp);
-  if (linelen != -1) {
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {
     while (linelen > 0 && (line[linelen - 1] == '\n' ||
                            line[linelen - 1] == '\r'))
       linelen--;
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+    editorAppendRow(line, linelen);
   }
   free(line);
   fclose(fp);
@@ -331,12 +343,12 @@ void editorDrawRows(struct abuf *ab) {
         while (padding--) abAppend(ab, " ", 1);
         abAppend(ab, welcome, welcomelen);
       } else {
-        abAppend(ab, "~", 1);
+        abAppend(ab, "~", 1); // like Vim ^^
       }
     } else {
-      int len = E.row.size;
+      int len = E.row[y].size;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row.chars, len);
+      abAppend(ab, E.row[y].chars, len);
     }
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
@@ -383,6 +395,7 @@ void initEditor()
   E.cx = 0;
   E.cy = 0;
   E.numrows = 0;
+  E.row = NULL;
 
   if(getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
