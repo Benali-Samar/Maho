746ab41acd872fba3e305bad2dd58e8a1ba5bd8a
BenAli Samar <samar.benali@istic.ucar.tn>
2023-10-21 23:09:19 +0100

    scroll back to the previous line with <-

--- maho.c
+++ maho.c
@@ -38,6 +38,7 @@ struct editorConfig {
   //for screen size ioctl request
   int cx,cy;
   int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -270,14 +271,24 @@ void abFree(struct abuf *ab)
 //Cursor moves 
 void editorMoveCursor(int key)
 {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch(key){
     case ARROW_LEFT:
       if (E.cx > 0){
         E.cx--;
+      } else if (E.cy > 0){
+        E.cy --;
+        E.cx = E.row[E.cy].size;
       }
       break;
     case ARROW_RIGHT:
-      if (E.cx < E.screencols -1) E.cx++;
+    if (row && E.cx < row -> size){
+      E.cx++;
+    } else if (row && E.cx == row -> size){
+      E.cy++;
+      E.cx = 0 ;
+    }
       break;
     case ARROW_UP:
       if (E.cy > 0)E.cy--;
@@ -286,6 +297,11 @@ void editorMoveCursor(int key)
       if (E.cy < E.screenrows ) E.cy ++;
       break;
   }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row -> size : 0;
+  if (E.cx > rowlen) E.cx = rowlen;
+
 }
 
 //Mapping the keypress to editor operations
@@ -332,6 +348,13 @@ void editorScroll() {
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+
+  if (E.cx < E.coloff){
+    E.coloff = E.cx;
+  }
+  if(E.cx >= E.coloff + E.screencols){
+    E.coloff = E.cx - E.screencols + 1 ;
+  }
 }
 
 
@@ -356,9 +379,10 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~", 1); // like Vim ^^
       }
     } else {
-      int len = E.row[filerow].size;
+      int len = E.row[filerow].size - E.coloff;
+      if (len < 0 ) len =0;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row[filerow].chars, len);
+      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
     }
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
@@ -384,7 +408,7 @@ void editorRefreshScreen()
 
   //position the cursor at the current position
   char buf[32];
-  snprintf(buf,sizeof(buf), "\x1b[%d;%dH",E.cy +1,E.cx +1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
   abAppend(&ab,buf,strlen(buf));
   
   //show cursor
@@ -406,6 +430,7 @@ void initEditor()
   E.cx = 0;
   E.cy = 0;
   E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
 
