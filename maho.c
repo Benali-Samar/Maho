3324178c09d282565fb7c697a1a5d44d0fdfc565
BenAli Samar <samar.benali@istic.ucar.tn>
2023-11-05 22:22:52 +0100

    Save to disque

--- maho.c
+++ maho.c
@@ -16,6 +16,7 @@
 #include <sys/types.h>
 #include <time.h>
 #include <stdarg.h>
+#include <fcntl.h>
 
 
 
@@ -57,12 +58,18 @@ struct editorConfig {
 
 struct editorConfig E;
 
+
+/***** prototypes *****/
+
+void editorSetStatusMessage(const char *fmt, ...);
+
 struct abuf {
   char *b;
   int len;
 };
 
 enum editorKey {
+  BACKSPACE = 127,
   ARROW_LEFT = 1000,
   ARROW_RIGHT ,
   ARROW_UP,
@@ -223,7 +230,7 @@ int getWindowSize(int *rows, int *cols)
 }
 
 
-/*** row operations ***/
+/***** row operations *****/
 
 int editorRowCxToRx(erow *row, int cx) {
   int rx = 0;
@@ -276,9 +283,47 @@ void editorAppendRow(char *s, size_t len)
   E.numrows++ ;
 }
 
+void editorRowInsertChar(erow *row, int at, int c){
+  if (at < 0 || at > row->size )
+    at = row -> size;
+  row -> chars = realloc(row -> chars, row -> size + 2);
+  memmove(&row -> chars[at + 1], &row -> chars[at], row -> size - at + 1);
+  row -> size ++;
+  row -> chars [at] = c;
+  editorUpdateRow(row);
+}
 
+
+/***** editor operations *****/
+void editorInsertChar (int c){
+  if (E.cy == E.numrows){
+    editorAppendRow("", 0);
+  }
+  editorRowInsertChar(&E.row[E.cy], E.cx, c);
+  E.cx++;
+}
 
 /*** file i/o ***/
+
+char *editorRowsToString(int *buflen){
+  int tolen = 0;
+  int j;
+  for (j =0; j < E.numrows; j++)
+    tolen += E.row[j].size + 1;
+  *buflen = tolen;
+
+  char *buf = malloc(tolen);
+  char *p = buf;
+  for (j = 0; j < E.numrows; j++){
+    memcpy(p, E.row[j].chars, E.row[j].size);
+    p += E.row[j].size;
+    *p = '\n';
+    p++;
+  }
+  return buf;
+  }
+
+
 void editorOpen(char *filename) {
   free(E.filename);
   E.filename = strdup(filename);
@@ -298,6 +343,29 @@ void editorOpen(char *filename) {
   free(line);
   fclose(fp);
 }
+
+
+void editorSave() {
+  if (E.filename == NULL) return;
+  int len;
+  char *buf = editorRowsToString(&len);
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+
+  if (fd != -1) {
+    if (ftruncate(fd, len) != -1) {
+      if (write(fd, buf, len) == len) {
+        close(fd);
+        free(buf);
+        editorSetStatusMessage("%d bytes written to disk", len);
+        return;
+      }
+    }
+    close(fd);
+  }
+  free(buf);
+  editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
 
 /***** Append buffer *****/ 
 
@@ -361,18 +429,35 @@ void editorMoveCursor(int key)
 void editorProcessKeypress() {
   int c = editorReadKey();
   switch (c) {
+    case '\r':
+      /* TODO */
+      break;
+
     case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
+    case CTRL_KEY('s'):
+      editorSave();
+      break;
+
     case HOME_KEY:
       E.cx = 0;
       break;
+
     case END_KEY:
       if (E.cy < E.numrows)
       E.cx = E.row[E.cy].size;
       break;
+
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL_KEY:
+      /* TODO */
+      break;
+
     case PAGE_UP:
     case PAGE_DOWN:
       {
@@ -387,11 +472,20 @@ void editorProcessKeypress() {
           editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
       }
       break;
+
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
     case ARROW_RIGHT:
       editorMoveCursor(c);
+      break;
+
+    case CTRL_KEY('l'):
+    case '\x1b':
+      break;
+
+    default:
+      editorInsertChar(c);
       break;
   }
 }
@@ -561,7 +655,7 @@ int main(int argc , char *argv[]) {
     editorOpen(argv[1]);
   }
   
-editorSetStatusMessage("HELP: Ctrl-Q = quit");
+editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
 
   while (1) {
     editorRefreshScreen();
