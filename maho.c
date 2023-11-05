854df5f917d299ed5eeb66d28d31ab5d1cdafc5d
BenAli Samar <samar.benali@istic.ucar.tn>
2023-11-05 22:43:49 +0100

    Backspacing, start and end

--- maho.c
+++ maho.c
@@ -286,6 +286,19 @@ void editorAppendRow(char *s, size_t len)
   E.dirty++;
 }
 
+void editorFreeRow(erow *row) {
+  free(row->render);
+  free(row->chars);
+}
+
+void editorDelRow(int at) {
+  if (at < 0 || at >= E.numrows) return;
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+  E.numrows--;
+  E.dirty++;
+}
+
 void editorRowInsertChar(erow *row, int at, int c){
   if (at < 0 || at > row->size )
     at = row -> size;
@@ -297,8 +310,27 @@ void editorRowInsertChar(erow *row, int at, int c){
   E.dirty++;
 }
 
+void editorRowAppendString(erow *row, char *s, size_t len) {
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
+
+void editorRowDelChar(erow *row, int at) {
+  if (at < 0 || at >= row->size) return;
+  memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+  row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
+}
 
 /***** editor operations *****/
+
+
 void editorInsertChar (int c){
   if (E.cy == E.numrows){
     editorAppendRow("", 0);
@@ -306,6 +338,25 @@ void editorInsertChar (int c){
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
 }
+
+
+void editorDelChar() {
+  if (E.cy == E.numrows) return;
+  if (E.cx == 0 && E.cy == 0) return;
+  erow *row = &E.row[E.cy];
+  if (E.cx > 0) {
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+    } else {
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
+  }
+}
+
+
+
 
 /*** file i/o ***/
 
@@ -470,7 +521,8 @@ void editorProcessKeypress() {
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
-      /* TODO */
+      if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+      editorDelChar();
       break;
 
     case PAGE_UP:
