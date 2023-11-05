d34c9371ff6b9e81e554c2b9df8a82d22020f5e9
BenAli Samar <samar.benali@istic.ucar.tn>
2023-11-05 23:00:28 +0100

    Save as .. feature

--- maho.c
+++ maho.c
@@ -64,6 +64,8 @@ struct editorConfig E;
 /***** prototypes *****/
 
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 struct abuf {
   char *b;
@@ -417,7 +419,13 @@ void editorOpen(char *filename) {
 
 
 void editorSave() {
-  if (E.filename == NULL) return;
+   if (E.filename == NULL) {
+      E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    if (E.filename == NULL) {
+      editorSetStatusMessage("Save aborted");
+      return;
+    }
+  }
   int len;
   char *buf = editorRowsToString(&len);
   int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
@@ -460,6 +468,38 @@ void abFree(struct abuf *ab)
 
 
 /***** Inputs *****/ 
+
+char *editorPrompt(char *prompt) {
+  size_t bufsize = 128;
+  char *buf = malloc(bufsize);
+  size_t buflen = 0;
+  buf[0] = '\0';
+  while (1) {
+    editorSetStatusMessage(prompt, buf);
+    editorRefreshScreen();
+
+    int c = editorReadKey();
+    if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE) {
+      if (buflen != 0) buf[--buflen] = '\0';
+    } else if (c == '\x1b') {
+      editorSetStatusMessage("");
+      free(buf);
+      return NULL;
+    } else if (c == '\r') {
+      if (buflen != 0) {
+        editorSetStatusMessage("");
+        return buf;
+      }
+    } else if (!iscntrl(c) && c < 128) {
+      if (buflen == bufsize - 1) {
+        bufsize *= 2;
+        buf = realloc(buf, bufsize);
+      }
+      buf[buflen++] = c;
+      buf[buflen] = '\0';
+    }
+  }
+}
 
 //Cursor moves 
 void editorMoveCursor(int key)
