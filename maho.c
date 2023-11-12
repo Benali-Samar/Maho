28cfe8039db7852d302c47b747e512324e41d155
BenAli Samar <samar.benali@istic.ucar.tn>
2023-11-12 01:50:38 +0100

    restore cursor position & search forward and backword

--- maho.c
+++ maho.c
@@ -464,26 +464,60 @@ void editorSave() {
 
 
 void editorFindCallback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+
   if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
     return;
+  }else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
   }
+
+  if (last_match == -1) direction = 1;
+  int current = last_match;
+
   int i;
   for (i = 0; i < E.numrows; i++) {
-    erow *row = &E.row[i];
-    char *match = strstr(row->render, query);
-    if (match) {
-      E.cy = i;
-      E.cx = editorRowRxToCx(row, match - row->render);
-      E.rowoff = E.numrows;
-      break;
-    }
+  current += direction;
+  if (current == -1) current = E.numrows - 1;
+  else if (current == E.numrows) current = 0;
+  erow *row = &E.row[current];
+  char *match = strstr(row->render, query);
+  if (match) {
+    last_match = current;
+    E.cy = current;
+    E.cx = editorRowRxToCx(row, match - row->render);
+    E.rowoff = E.numrows;
+    break;
+  }
   }
 }
+
+
+
 void editorFind() {
-  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+  int saved_cx = E.cx;
+  int saved_cy = E.cy;
+  int saved_coloff = E.coloff;
+  int saved_rowoff = E.rowoff;
+
+  char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
+
   if (query) {
     free(query);
-  }
+  }else {
+    E.cx = saved_cx;
+    E.cy = saved_cy;
+    E.coloff = saved_coloff;
+    E.rowoff = saved_rowoff;
+}
 }
 
 /***** Append buffer *****/ 
